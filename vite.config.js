f4dd8e083d0d75dd4a78e9e8695835a1a2a2090a
14_12310160_Gohan Tua Jeremia Ambarita <gohan.123140160@student.itera.ac.id>
2025-11-02 23:48:11 +0700

    Initial commit - Final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', 
+  root: '.', 
+  publicDir: 'public', 
+  
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true,
+    
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]", 
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
