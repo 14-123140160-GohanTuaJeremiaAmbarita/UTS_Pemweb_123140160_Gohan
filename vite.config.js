abcc12b73712b3c475fa73fe42dcc19fa7ce761b
14_12310160_Gohan Tua Jeremia Ambarita <gohan.123140160@student.itera.ac.id>
2025-11-02 23:22:04 +0700

    Final3

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs/promises";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "swap-index-html",
-      enforce: "pre",
-      async transformIndexHtml(html) {
-        const alt = await fs.readFile(path.resolve(__dirname, "public/index.html"), "utf-8");
-        return alt;
-      }
-    }
-  ],
-  server: {
-    port: 5173
-  }
-});
+  plugins: [react()],
+})
